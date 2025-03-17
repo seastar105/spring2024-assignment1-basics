a88882f11e96fc2713ee50758d6cfdb8eb422931
Haesung Jeon <seastar105@gmail.com>
2025-03-17 11:55:44 +0900

    pre-commit

--- cs336_basics/bbpe.py
+++ cs336_basics/bbpe.py
@@ -1,10 +1,11 @@
-import sys
-from typing import List, Tuple, Dict
-import regex as re
-import time
 import logging
-from contextlib import contextmanager
+import sys
+import time
 from collections import defaultdict
+from contextlib import contextmanager
+from typing import Dict, List, Tuple
+
+import regex as re
 from tqdm.auto import tqdm
 
 logger = logging.getLogger(__name__)
@@ -23,11 +24,12 @@ def timer(name):
     end = time.perf_counter()
     logger.error(f"{name} took {end - start:.2f}s")
 
+
 class BBPE:
     def __init__(self, special_tokens: List[str] = []):
-        self.pattern = """'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+        self.pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
         self.compiled_pattern = re.compile(self.pattern)
-        
+
         self.special_tokens = special_tokens
         self.vocab = dict()
         for i, token in enumerate(self.special_tokens):
@@ -36,23 +38,23 @@ class BBPE:
             self.vocab[256 + i] = bytes([i])
         self.merges: List[Tuple[bytes, bytes]] = []
         self.special_tokens = special_tokens
-    
+
     def train(self, input_path: str, vocab_size: int, special_tokens: List[str], progress: bool = False):
         with timer("Reading corpus"):
-            with open(input_path, "r") as f:
+            with open(input_path) as f:
                 corpus = f.read()
-        
+
         with timer("Pre-tokenizing"):
             chunks = self.pre_tokenize(corpus)
-        
+
         with timer("Training BPE"):
             vocab = dict()
             for i in range(256):
                 vocab[i] = bytes([i])
-            
+
             merges = []
             num_merges = vocab_size - len(vocab) - len(special_tokens)
-            
+
             iterator = tqdm(range(num_merges)) if progress else range(num_merges)
             for merge_idx in iterator:
                 occurences = defaultdict(int)
@@ -62,12 +64,12 @@ class BBPE:
                         occurences[(p1, p2)] += 1
                 # choose most frequence pair, tie breaking rule is lexicographical order
                 max_pair = max(occurences, key=lambda x: (occurences[x], vocab[x[0]], vocab[x[1]]))
-                
+
                 # add to vocab and merge
                 idx = 256 + merge_idx
                 merges.append((vocab[max_pair[0]], vocab[max_pair[1]]))
                 vocab[idx] = vocab[max_pair[0]] + vocab[max_pair[1]]
-                
+
                 # replace all occurences of max_pair with idx
                 new_chunks = []
                 for chunk in chunks:
@@ -86,14 +88,13 @@ class BBPE:
         new_vocab = dict()
         for i, token in enumerate(special_tokens):
             new_vocab[i] = token.encode("utf-8")
-        
+
         for i, token in vocab.items():
             new_vocab[i + len(special_tokens)] = token
-        
+
         self.vocab = new_vocab
         self.merges = merges
         self.special_tokens = special_tokens
-            
 
     def pre_tokenize(self, corpus: str) -> List[List[int]]:
         # is corpus a string? or list of strings?
